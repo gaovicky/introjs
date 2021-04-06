21f6e57570aa7eddb3375e59c6c66a81a9d2ceb4
gaovicky <vg1234@nyu.edu>
2021-04-06 09:57:52 -0400

    revision

--- 03-functionCombine.js
+++ 03-functionCombine.js
@@ -1,15 +1,23 @@
 
 /*Setting up a function with arguments*/
-function compoundWords (wordOne, wordTwo) {
-  console.log ("Compound Word: " + (wordOne + wordTwo));
-}
+//function compoundWords (wordOne, wordTwo) {
+  //console.log ("Compound Word: " + (wordOne + wordTwo));
+//}
 
 /*Choosing words for the compoundWords function*/
-compoundWords ("sun", "flower")
+//compoundWords ("sun", "flower")
 
 /*Return a concatenated string*/
+//function compoundWordsandreturn (wordOne, wordTwo) {
+  //return wordOne + wordTwo;
+//}
+
+//console.log (compoundWordsandreturn ("snow", "flake"))
+
+/*--revision--*/
 function compoundWordsandreturn (wordOne, wordTwo) {
-  return wordOne + wordTwo;
+  var sentence = "Compound word: " + (wordOne + wordTwo);
+  return sentence;
 }
 
 console.log (compoundWordsandreturn ("snow", "flake"))
--- 05-randomWords.js
+++ 05-randomWords.js
@@ -2,7 +2,11 @@
 /*Array of Strings*/
 var CarBrands = ["Acura", "BMW", "Chrysler", "Dodge", "Ford", "Genesis", "Honda", "Infiniti"];
 
+/*Generate random whole number between 0-7*/
 var randomIndex = Math.floor( Math.random() * CarBrands.length);
 
+/*Create a sentence*/
+var fullSentence = CarBrands[randomIndex] + " is a car brand.";
+
 /*Use random index to choose a random car brand from the array*/
-console.log(CarBrands[randomIndex]);
+console.log(fullSentence);
